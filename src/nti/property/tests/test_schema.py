20e1effd196446c1d5ed497f5a9ec190db018abf
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-12-05 19:00:24 +0000

    add test
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.property/trunk@101997 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/property/tests/test_schema.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+# disable: accessing protected members, too many methods
+# pylint: disable=W0212,R0904
+
+from hamcrest import is_
+from hamcrest import assert_that
+
+import unittest
+
+from nti.property.schema import DataURI
+
+GIF_DATAURL = b'data:image/gif;base64,R0lGODlhCwALAIAAAAAA3pn/ZiH5BAEAAAEALAAAAAALAAsAAAIUhA+hkcuO4lmNVindo7qyrIXiGBYAOw=='
+
+class TestSchema(unittest.TestCase):
+
+	def test_data_url_class(self):
+		value = DataURI.is_valid_data_uri(GIF_DATAURL)
+		assert_that(value, is_(True))
+		data_url = DataURI(__name__='url').fromUnicode(GIF_DATAURL)
+		assert_that(data_url, is_(GIF_DATAURL))
